cbd10c61a75f9eddb36c83dc82310da1eacfb5e9
zEphyr1801 <shandykaputraone1807@gmail.com>
2024-12-26 19:33:10 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,24 @@
+int = 1 # 1 - 999+
+
+str = 'Halo Guys'
+
+float = 1.0 # 1.0 - 999.0+
+
+bool = True or False
+
+list = ['nilai satu', 2, 3.0, 'nilai 4']
+list.append(...) # Jika anda ingin menambahkan nilai
+list.remove(...) # Jika anda ingin mengurangi nilai
+list[0] = 'nilai nol' # Jika anda ingin mengubah nilai. index python dimulai dari 0.
+
+tuple = ('nilai satu', 2, 3.0, 'nilai 4')
+
+set = {'nilai satu', 2, 3.0, 'nilai 4'}
+
+dictionary = {
+    'kunci': 'nilai',
+    'keys': 23
+              }
+
+
+ 
